f9f2cfe06e2abdcd36bd69364d302f1d61dbcea8
Federico Navarrete <fanmixco@gmail.com>
2026-05-22 07:23:44 +0200

    Fixed analytics

--- js/gAnalyticsLoad.js
+++ js/gAnalyticsLoad.js
@@ -1,11 +1,29 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    let gaScript = document.createElement("script");
-    gaScript.src = "https://www.googletagmanager.com/gtag/js?id=G-FHG5GPZL6Q";
-    gaScript.async = true;
-    gaScript.defer = true;
-    document.body.appendChild(gaScript);
+(function loadGoogleAnalytics() {
+  const measurementId = "G-FHG5GPZL6Q";
 
-    let customScript = document.createElement("script");
-    customScript.src = "js/analytics.js";
-    document.body.appendChild(customScript);
-});
+  function initGoogleAnalytics() {
+    window.dataLayer = window.dataLayer || [];
+    window.gtag =
+      window.gtag ||
+      function gtag() {
+        window.dataLayer.push(arguments);
+      };
+
+    window.gtag("js", new Date());
+    window.gtag("config", measurementId);
+
+    const gaScript = document.createElement("script");
+    gaScript.src =
+      "https://www.googletagmanager.com/gtag/js?id=" + measurementId;
+    gaScript.async = true;
+    document.head.appendChild(gaScript);
+  }
+
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", initGoogleAnalytics, {
+      once: true,
+    });
+  } else {
+    initGoogleAnalytics();
+  }
+})();
